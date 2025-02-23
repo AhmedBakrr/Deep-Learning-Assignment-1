12361691759ad43d20545e6ac013773f7b206e51
Ahmed Bakr <100361500+AhmedBakrr@users.noreply.github.com>
2025-02-23 07:51:38 +0200

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
+import torch.optim as optim  # For all Optimization algorithms, SGD, Adam, etc.
+import torch.nn.functional as F  # All functions that don't have any parameters
+from torch.utils.data import DataLoader  # Gives easier dataset management and creates mini batches
+from torch.optim import lr_scheduler
+from torchvision import datasets, models, transforms
+
+# Device configuration
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Hyperparameters
+num_classes = 10
+learning_rate = 1e-3
+batch_size = 64
+num_epochs = 5
+
+# EuroSAT dataset
+train_dataset = datasets.EuroSAT(root='./data', transform=transforms.ToTensor(), download=True)
+test_dataset = datasets.EuroSAT(root='./data', transform=transforms.ToTensor(), download=True)
+
+# Data loader
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+# Loading pre-trained DenseNet121 and modifying final layer
+model = models.densenet121(pretrained=True)
+num_ftrs = model.classifier.in_features
+model.classifier = nn.Linear(num_ftrs, num_classes)
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+# Fine-tuning: Freeze all layers except the final layer
+model_conv = models.densenet121(pretrained=True)
+for param in model_conv.parameters():
+    param.requires_grad = False
+num_ftrs = model_conv.classifier.in_features
+model_conv.classifier = nn.Linear(num_ftrs, num_classes)
+model_conv = model_conv.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer_conv = optim.SGD(model_conv.classifier.parameters(), lr=0.001, momentum=0.9)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+# Training loop
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        model.train()
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 10 == 0:
+            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+    
+    exp_lr_scheduler.step()
+
+# Evaluation
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        model.eval()
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network on the test images: {acc} %')
